d16d9665d43978d5e184434541a4171cce1f824b
Limbo395 <144790046+Limbo395@users.noreply.github.com>
2024-01-04 00:17:20 +0200

    0.1 (base save)

---
+++ main.py
@@ -0,0 +1,169 @@
+from tkinter import messagebox
+from sql import *
+import tkinter as tk
+from tkinter import ttk
+
+#Створити дерево за параметрами
+def make_tree(window, rows):
+    tree = ttk.Treeview(window, show="headings")
+    tree.config(height=15)
+    tree["columns"] = tuple(rows[0].keys())
+    for col in tree["columns"]:
+        tree.heading(col, text=col)
+        tree.column(col, anchor=tk.CENTER)
+    for row in rows:
+        tree.insert("", "end", values=tuple(row.values()))
+    return tree
+
+
+
+#Функції для кнопока
+def button_click_for_table(root, variant):
+    def on_closing():
+        root.deiconify()
+        window.destroy()
+    def edit_row(window):
+        def on_closing_third_window():
+            window.deiconify()
+            edit_window.destroy()
+            return
+        def save_changes():
+            new_values = [entry.get() for entry in entries]
+            update_row(variant, new_values)
+            tree.item(selected_item, values=new_values)
+            window.deiconify()
+            edit_window.destroy()
+        window.withdraw()
+        selected_item = tree.selection()
+        if not selected_item:
+            messagebox.showwarning("Edit", "Please select a row to edit.")
+            window.deiconify()
+            return
+
+        columns = tree['columns']
+        values = tree.item(selected_item)['values']
+
+        edit_window = tk.Toplevel(window)
+        edit_window.title("Edit Row")
+        edit_window.protocol("WM_DELETE_WINDOW", lambda: on_closing_third_window())
+
+        entries = []
+        for index, (column, value) in enumerate(zip(columns, values)):
+            tk.Label(edit_window, text=f"{column}:", font=("DIN Condensed Bold (Body)", 14)).grid(row=index, column=0, padx=5, pady=5)
+            entry = tk.Entry(edit_window, width=70)
+            entry.insert(0, value)
+            entry.grid(row=index, column=1, padx=5, pady=5)
+            if index == 0:
+                entry.config(state="disabled")
+            entries.append(entry)
+        save_button = tk.Button(edit_window, text="Save", command=save_changes)
+        save_button.grid(row=len(columns), column=0, columnspan=2, pady=10)
+
+    def delete_row():
+        selected_item = tree.selection()
+        if not selected_item:
+            messagebox.showwarning("Delete", "Please select a row to delete.")
+            window.deiconify()
+            return
+
+        confirm = messagebox.askyesno("Delete", "Are you sure you want to delete the selected row?")
+        if confirm:
+            row_id = tree.item(selected_item)['values'][0]
+            delete_row_table(variant, row_id)
+            tree.delete(selected_item)
+
+    def add_new_row():
+        def on_closing_third_window():
+            window.deiconify()
+            add_window.destroy()
+            return
+        def add_new():
+            new_values = [entry.get() for entry in entries]
+            insert_row(variant, new_values)  # pass values except for the ID
+            tree.insert("", "end", values=new_values)
+            window.deiconify()
+            add_window.destroy()
+
+        window.withdraw()
+        add_window = tk.Toplevel(window)
+        add_window.title("Add New Row")
+        add_window.protocol("WM_DELETE_WINDOW", lambda: on_closing_third_window())
+
+        entries = []
+        columns = tree['columns']
+        for index, column in enumerate(columns):
+            tk.Label(add_window, text=f"{column}:", font=("DIN Condensed Bold (Body)", 14)).grid(row=index, column=0, padx=5, pady=5)
+            entry = tk.Entry(add_window, width=70)
+            if index == 0:
+                existing_values = set(tree.item(item_id)['values'][0] for item_id in tree.get_children())
+                new_value = 1
+                while new_value in existing_values:
+                    new_value += 1
+                entry.insert(0, new_value)
+                entry.config(state="disabled")
+            entries.append(entry)
+            entry.grid(row=index, column=1, padx=5, pady=5)
+
+        add_button = tk.Button(add_window, text="Add", command=add_new)
+        add_button.grid(row=len(columns), column=0, columnspan=2, pady=10)
+
+    root.withdraw()
+    window = tk.Toplevel(root)
+    window.title("Таблиця з бази даних")
+    window.protocol("WM_DELETE_WINDOW", lambda: on_closing())
+
+    match variant:
+        case "BRV":
+            tree = make_tree(window, show_all("BRV"))
+            tree.pack()
+        case "RRV":
+            tree = make_tree(window, show_all("RRV"))
+            tree.pack()
+        case "TRV":
+            tree = make_tree(window, show_all("TRV"))
+            tree.pack()
+        case "DIV":
+            tree = make_tree(window, show_all("DIV"))
+            tree.pack()
+        case "Storages":
+            tree = make_tree(window, show_all("Storages"))
+            tree.pack()
+
+    button_change = tk.Button(window, font=("DIN Condensed Bold (Body)", 26), text="Edit", command=lambda: edit_row(window))
+    button_change.pack(side=tk.LEFT, padx=50, pady=20)
+
+    button_delete = tk.Button(window, font=("DIN Condensed Bold (Body)", 26), text="Delete", command=delete_row)
+    button_delete.pack(side=tk.RIGHT, padx=50, pady=20)
+
+    button_add = tk.Button(window, font=("DIN Condensed Bold (Body)", 26), text="Add new", command=add_new_row)
+    button_add.pack(side=tk.RIGHT, pady=20)
+
+
+
+
+
+#Головне вікно з кнопками
+root = tk.Tk()
+root.title("Таблиця з бази даних")
+
+button_BRV = tk.Button(root, text="Показати BRV", command=lambda: button_click_for_table(root, "BRV"))
+button_BRV.pack(pady=10)
+
+button_RRV = tk.Button(root, text="Показати RRV", command=lambda: button_click_for_table(root, "RRV"))
+button_RRV.pack(pady=10)
+
+button_TRV = tk.Button(root, text="Показати TRV", command=lambda: button_click_for_table(root, "TRV"))
+button_TRV.pack(pady=10)
+
+button_DIV = tk.Button(root, text="Показати DIV", command=lambda: button_click_for_table(root, "DIV"))
+button_DIV.pack(pady=10)
+
+button_Storages = tk.Button(root, text="Показати Storages", command=lambda: button_click_for_table(root, "Storages"))
+button_Storages.pack(pady=10)
+
+
+
+
+root.resizable(False, False)
+root.mainloop()
+
---
+++ sql.py
@@ -0,0 +1,95 @@
+import pymysql
+from config import host, user, password, db_name
+
+try:
+    conection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("Successfully connected...")
+    print("#" * 20)
+
+
+
+    def show_all(variant):
+            with conection.cursor() as cursor:
+                match variant:
+                    case "BRV":
+                        cursor.execute("SELECT * from `BRV`")
+                    case "RRV":
+                        cursor.execute("SELECT * from `RRV`")
+                    case "TRV":
+                        cursor.execute("SELECT * from `TRV`")
+                    case "DIV":
+                        cursor.execute("SELECT * from `SourcesOfIonizingRadiation`")
+                    case "Storages":
+                        cursor.execute("SELECT * from `Storages`")
+                return cursor.fetchall()
+    def update_row(variant, values):
+        with conection.cursor() as cursor:
+            match variant:
+                case "BRV":
+                    update_query = "UPDATE `BRV` SET ID_Of_BRV = %s, ID_OfStorage = %s, GroupOf_BRV = %s, GeneralActivity = %s, ID_OfRadionuclideDirectory = %s, DateOfReceipt = %s, ManufacturerOrSource = %s, ID_StorageMethod = %s WHERE ID_Of_BRV = %s"
+                case "RRV":
+                    update_query = "UPDATE `RRV` SET ID_Of_RRV = %s, ID_OfStorage = %s, CategoryOf_RRV = %s, GeneralActivity = %s, ID_OfRadionuclideDirectory = %s, DateOfReceipt = %s, ManufacturerOrSource = %s, ID_StorageMethod = %s WHERE ID_Of_RRV = %s"
+                case "TRV":
+                    update_query = "UPDATE `TRV` SET ID_Of_TRV = %s, ID_OfStorage = %s, GroupOf_TRV = %s, GeneralActivity = %s, ID_OfRadionuclideDirectory = %s, DateOfReceipt = %s, ManufacturerOrSource = %s, ID_StorageMethod = %s WHERE ID_Of_TRV = %s"
+                case "DIV":
+                    update_query = "UPDATE `SourcesOfIonizingRadiation` SET ID_SourcesOfIonizingRadiation = %s, ID_OfStorage = %s, ID_OfRadionuclideDirectory = %s, Activity = %s, DateOfManufacture = %s, DateOfReceipt = %s, TermsOfServiceOfSIR = %s, NameOfTheDevice = %s, ManufucturerOrSource = %s, ID_StorageMethod = %s WHERE ID_SourcesOfIonizingRadiation = %s"
+                case "Storages":
+                    update_query = "UPDATE `Storages` SET NameOfStorage = %s, Location = %s, StorageCondition = %s, TypeOfStorageID = %s, StorageVolume = %s, CertificateID = %s, DateOfVerification = %s,  WHERE ID = %s"
+            values.append(values[0]) 
+            cursor.execute(update_query, values)
+            conection.commit()
+
+    def delete_row_table(variant, row_id):
+        with conection.cursor() as cursor:
+            match variant:
+                case "BRV":
+                    delete_query = "DELETE FROM `BRV` WHERE ID_Of_BRV = %s"
+                case "RRV":
+                    delete_query = "DELETE FROM `RRV` WHERE ID_Of_RRV = %s"
+                case "TRV":
+                    delete_query = "DELETE FROM `TRV` WHERE ID_Of_TRV = %s"
+                case "DIV":
+                    delete_query = "DELETE FROM `SourcesOfIonizingRadiation` WHERE ID_SourcesOfIonizingRadiation = %s"
+                case "Storages":
+                    delete_query = "DELETE FROM `Storages` WHERE ID = %s"
+            cursor.execute(delete_query, (row_id,))
+            conection.commit()
+
+    def insert_row(variant, values):
+        with conection.cursor() as cursor:
+            match variant:
+                case "BRV":
+                    insert_query = "INSERT INTO `BRV` (ID_Of_BRV, ID_OfStorage, GroupOf_BRV, GeneralActivity, ID_OfRadionuclideDirectory, DateOfReceipt, ManufacturerOrSource, Id_StorageMethod) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                case "RRV":
+                    insert_query = "INSERT INTO `RRV` (ID_Of_RRV, ID_OfStorage, CategoryOf_RRV, GeneralActivity, ID_OfRadionuclideDirectory, DateOfReceipt, ManufacturerOrSource, Id_StorageMethod) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                case "TRV":
+                    insert_query = "INSERT INTO `TRV` (ID_Of_TRV, ID_OfStorage, GroupOf_TRV, GeneralActivity, ID_OfRadionuclideDirectory, DateOfReceipt, ManufacturerOrSource, Id_StorageMethod) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                case "DIV":
+                    insert_query = "INSERT INTO `SourcesOfIonizingRadiation` (ID_SourcesOfIonizingRadiation, ID_OfStorage, ID_OfRadionuclideDirectory, Activity, DateOfManufacture, DateOfReceipt, TermsOfServiceOfSIR, Name Of The Device, ManufuctureOrSource, Id_StorageMethod) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                case "Storages":
+                    insert_query = "INSERT INTO `Storages` (ID, NameOfStorage, Location, StorageCondition, TypeOfStorageID, StorageVolume, CertificateID, DateOfVerification) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(insert_query, values)
+            conection.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+except Exception as ex:
+    print("Conection refused...")
+    print(ex)
+    exit(0)
